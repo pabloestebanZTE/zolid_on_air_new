51cf06273f78647a648bb79169b9663b183914ed
Jarvanux <john.vanegas67@gmail.com>
2017-11-09 09:42:34 -0500

    Se actualiza el coordinador.js

--- assets/js/modules/principal/coordinador.js
+++ assets/js/modules/principal/coordinador.js
@@ -38,7 +38,7 @@ $(function () {
         },
         getButtonsPending: function (obj) {
             return '<div class="btn-group">'
-                    + '<a href="' + app.urlTo('User/trackingDetails') + '" class="btn btn-default btn-xs" data-toggle="tooltip" title="Detalle"><span class="fa fa-fw fa-eye"></span></a>'
+                    + '<a href="' + app.urlTo('User/trackingDetails?' + obj.k_id_onair) + '" class="btn btn-default btn-xs" data-toggle="tooltip" title="Detalle"><span class="fa fa-fw fa-eye"></span></a>'
                     + '<a href="' + app.urlTo('User/assignEngineer?idOnair=' + obj.k_id_onair) + '" class="btn btn-default btn-xs" data-toggle="tooltip" title="Asignar"><span class="fa fa-fw fa-sign-in"></span></a>'
                     + '</div>';
         },
