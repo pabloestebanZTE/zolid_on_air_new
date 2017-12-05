5baccd38009e972cb0997a430e150a621b8c883d
Jarvanux <john.vanegas67@gmail.com>
2017-12-05 16:40:50 -0500

    Se actualiza una cosilla...

--- application/controllers/Documenter.php
+++ application/controllers/Documenter.php
@@ -89,7 +89,7 @@ class Documenter extends CI_Controller {
     public function updateDetails() {
         $ticket = new dao_ticketOnAir_model();
         $preparation = new dao_preparationStage_model();
-        $response = $ticket->updatePrecheckOnair($this->request, 78);
+        $response = $ticket->updatePrecheckOnair($this->request);
         $this->request->k_id_preparation = $this->request->k_id_prep;
         $response = $preparation->updatePreparationStage($this->request);
         $this->json($response);
