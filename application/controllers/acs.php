cadb2f7ebb81e1c759e9be77e82b6994c96d9cba
Jarvanux <john.vanegas67@gmail.com>
2017-12-13 11:55:31 -0500

    asfafasfa

---
+++ application/controllers/acs.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Acs extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    public function principal() {
+        if (Auth::check()) {
+            Redirect::to(URL::base());
+        }
+        $this->load->view('principalvm');
+    }
+
+    public function acsview() {
+        if (Auth::check()) {
+            Redirect::to(URL::base());
+        }
+        $this->load->view('acsView');
+    }
+
+}
