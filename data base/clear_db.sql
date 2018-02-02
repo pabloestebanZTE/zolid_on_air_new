352ed5cd44b4d5a2b2f159b4fb7a0bdf4a2fc66b
jhonjaider1000 <jhonjaider1000@jhonjaider1000-PCSGOB14p-C>
2018-02-02 00:02:19 -0500

    Ok

---
+++ data base/clear_db.sql
@@ -0,0 +1,12 @@
+truncate table on_air_12h;
+truncate table on_air24h;
+truncate table on_air_36h;
+TRUNCATE `scaled_on_air`;
+TRUNCATE table audit_on_air;
+TRUNCATE `reporte_comentario`;
+delete from ticket_on_air;
+delete from preparation_stage;
+delete from precheck;
+delete from follow_up_12h;
+delete from follow_up_24h;
+delete from follow_up_36h;
