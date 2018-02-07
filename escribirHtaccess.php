af472c1364a703859fa049405433c06f5d1db964
jhonjaider1000 <jhonjaider1000@jhonjaider1000-Lenovo-V310-14ISK>
2018-02-07 13:48:04 -0500

    ok

--- escribirHtaccess.php
+++ escribirHtaccess.php
@@ -1,7 +1,9 @@
-RewriteEngine on
+<?php
+
+file_put_contents(".htaccess", "RewriteEngine on
 RewriteCond %{REQUEST_FILENAME} !-f
 RewriteCond %{REQUEST_FILENAME} !-d
 RewriteRule ^(.*)$ index.php [NC,L,QSA]
 
 php_value upload_max_filesize 100M
-php_value post_max_size 100M
+php_value post_max_size 100M");
